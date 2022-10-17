e03d24c388d3e2a4ac436c384777a4badce24201
kuso-wedding <115979351+kuso-wedding@users.noreply.github.com>
2022-10-18 00:13:15 +0900

    Delete wedding directory

--- wedding/js/photoswipe.esm.js
+++
@@ -1,9 +0,0 @@
-<script type="module">
-import PhotoSwipeLightbox from 'photoswipe/dist/photoswipe-lightbox.esm.js';
-const lightbox = new PhotoSwipeLightbox({
-  gallery: '#my-gallery',
-  children: 'a',
-  pswpModule: () => import('photoswipe/dist/photoswipe.esm.js')
-});
-lightbox.init();
-</script>
